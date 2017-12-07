ec139ad94fd7cf594ccfffb9adb4a9b07a1f7987
masterhui <masterhui@users.noreply.github.com>
2017-12-07 12:46:20 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -9,15 +9,25 @@ angular.module('d3', []);
 angular.module('greenPiThumbApp.directives', ['d3']);
 
 // Input: datetime sting with format "20171207T0610Z"
-// Return: datetime sting with format 07.12.2017 06:10"
-function formatDateTimeString(dateTimeStr) {
+// Return: javascript date object
+function createDateFromString(dateTimeStr) {
   var year = dateTimeStr.substring(0, 4);
   var month = dateTimeStr.substring(4, 6);
   var day = dateTimeStr.substring(6, 8);
   var hour = dateTimeStr.substring(9, 11);
-  var minute = dateTimeStr.substring(11, 13);
-  
-  return day + "." + month + "." + year + " " + hour + ":" + minute
+  var minute = dateTimeStr.substring(11, 13);  
+  var d = new Date(year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":00Z" );  
+  return d
+}
+
+// Input: datetime sting with format "20171207T1410Z"
+// Return: Date string with format "Do., 7.12.2017 15:10"
+function formatDate(date_str) {
+	var date = createDateFromString(date_str);
+	var date_options = { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' };
+  var date_formatted = date.toLocaleDateString("en-EN", date_options);
+  var date_time = date_formatted + " " + date.getHours() + ":" + date.getMinutes();    
+  return date_time;
 }
 
 greenPiThumbApp.controller('DashboardCtrl', function($scope, $http) {
@@ -25,47 +35,47 @@ greenPiThumbApp.controller('DashboardCtrl', function($scope, $http) {
     $scope.temperature = temperatureHistory;
     $scope.temperatureRecords = temperatureHistory.length;
     $scope.latestTemperature = temperatureHistory[temperatureHistory.length - 1].temperature;
-    $scope.temperatureTimestamp = temperatureHistory[temperatureHistory.length - 1].timestamp;
+    $scope.temperatureTimestamp = formatDate(temperatureHistory[temperatureHistory.length - 1].timestamp);
   });
   $http.get('/waterLevelHistory.json').success(function(waterLevelHistory) {    
     $scope.waterLevel = waterLevelHistory;
     $scope.waterLevelRecords = waterLevelHistory.length;
     $scope.latestWaterLevel = waterLevelHistory[waterLevelHistory.length - 1].water_level;
-    $scope.waterLevelTimestamp = waterLevelHistory[waterLevelHistory.length - 1].timestamp;
+    $scope.waterLevelTimestamp = formatDate(waterLevelHistory[waterLevelHistory.length - 1].timestamp);
   });  
   $http.get('/humidityHistory.json').success(function(humidityHistory) {
     $scope.humidity = humidityHistory;
     $scope.humidityRecords = humidityHistory.length;
     $scope.latestHumidity = humidityHistory[humidityHistory.length - 1].humidity;
-    $scope.humidityTimestamp = humidityHistory[humidityHistory.length - 1].timestamp;	  
+    $scope.humidityTimestamp = formatDate(humidityHistory[humidityHistory.length - 1].timestamp);	  
   });
   $http.get('/lightHistory.json').success(function(lightHistory) {
     $scope.lightLevel = lightHistory;
     $scope.lightRecords = lightHistory.length;
     $scope.latestLightLevel = lightHistory[lightHistory.length - 1].light;
-    $scope.lightLevelTimestamp = lightHistory[lightHistory.length - 1].timestamp;	  
+    $scope.lightLevelTimestamp = formatDate(lightHistory[lightHistory.length - 1].timestamp);	  
   });
   $http.get('/soilMoistureHistory.json').success(function(moistureHistory) {
     $scope.soilMoisture = moistureHistory;
     $scope.soilMoistureRecords = $scope.soilMoisture.length;    
     $scope.latestSoilMoisture = $scope.soilMoisture[$scope.soilMoisture.length - 1].soil_moisture;
-    $scope.soilMoistureTimestamp = $scope.soilMoisture[$scope.soilMoisture.length - 1].timestamp;	  
+    $scope.soilMoistureTimestamp = formatDate($scope.soilMoisture[$scope.soilMoisture.length - 1].timestamp);	  
   });
   $http.get('/wateringEventHistory.json').success(function(wateringEventHistory) {
     $scope.waterPumped = wateringEventHistory;    
     if (wateringEventHistory.length >= 1) {
         $scope.latestPumpEvent1 = wateringEventHistory[wateringEventHistory.length - 1].water_pumped;
-        $scope.pumpEventTimestamp1 = wateringEventHistory[wateringEventHistory.length - 1].timestamp;
+        $scope.pumpEventTimestamp1 = formatDate(wateringEventHistory[wateringEventHistory.length - 1].timestamp);
         $scope.waterRecords1 = wateringEventHistory.length;
     }
     if (wateringEventHistory.length >= 2) {
         $scope.latestPumpEvent2 = wateringEventHistory[wateringEventHistory.length - 2].water_pumped;
-        $scope.pumpEventTimestamp2 = wateringEventHistory[wateringEventHistory.length - 2].timestamp;
+        $scope.pumpEventTimestamp2 = formatDate(wateringEventHistory[wateringEventHistory.length - 2].timestamp);
         $scope.waterRecords2 = wateringEventHistory.length - 1;
     }
     if (wateringEventHistory.length >= 3) {
         $scope.latestPumpEvent3 = wateringEventHistory[wateringEventHistory.length - 3].water_pumped;
-        $scope.pumpEventTimestamp3 = wateringEventHistory[wateringEventHistory.length - 3].timestamp;
+        $scope.pumpEventTimestamp3 = formatDate(wateringEventHistory[wateringEventHistory.length - 3].timestamp);
         $scope.waterRecords3 = wateringEventHistory.length - 2;
     }
   });	
