53aadd06db691cc6cdb1991ee257f39f71e2d630
masterhui <masterhui@users.noreply.github.com>
2017-12-07 11:53:55 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -8,15 +8,27 @@ var greenPiThumbApp = angular.module('greenPiThumbApp', [
 angular.module('d3', []);
 angular.module('greenPiThumbApp.directives', ['d3']);
 
+// Input: datetime sting with format "20171207T0610Z"
+// Return: datetime sting with format 07.12.2017 06:10"
+function formatDateTimeString(dateTimeStr) {
+  var year = dateTimeStr.substring(0, 4);
+  var month = dateTimeStr.substring(4, 6);
+  var day = dateTimeStr.substring(6, 8);
+  var hour = dateTimeStr.substring(9, 11);
+  var minute = dateTimeStr.substring(11, 13);
+  
+  return day + "." + month + "." + year + " " + hour + ":" + minute
+}
+
 greenPiThumbApp.controller('DashboardCtrl', function($scope, $http) {
   $http.get('/temperatureHistory.json').success(function(temperatureHistory) {    
-    $scope.temperature = temperatureHistory
+    $scope.temperature = temperatureHistory;
     $scope.temperatureRecords = temperatureHistory.length;
     $scope.latestTemperature = temperatureHistory[temperatureHistory.length - 1].temperature;
     $scope.temperatureTimestamp = temperatureHistory[temperatureHistory.length - 1].timestamp;
   });
   $http.get('/waterLevelHistory.json').success(function(waterLevelHistory) {    
-    $scope.waterLevel = waterLevelHistory
+    $scope.waterLevel = waterLevelHistory;
     $scope.waterLevelRecords = waterLevelHistory.length;
     $scope.latestWaterLevel = waterLevelHistory[waterLevelHistory.length - 1].water_level;
     $scope.waterLevelTimestamp = waterLevelHistory[waterLevelHistory.length - 1].timestamp;
