eb456ce580bed06c9f6e32516e7684c45c479444
Heiko <heikohelble@gmail.com>
2017-12-13 01:04:44 +0100

    Movable charts 1st

--- app/directives/lineGraph.js
+++ app/directives/lineGraph.js
@@ -29,8 +29,8 @@ angular.module('greenPiThumbApp.directives')
           var height = 450 - margin.top - margin.bottom;
 
           var parseTimestamp = d3.utcParse('%Y%m%dT%H%M%Z');
-
-          // Set the ranges
+    
+          // Set the ranges    
           var x = d3.scaleTime().range([0, width]);
           var y = d3.scaleLinear().range([height, 0]);
 
@@ -46,6 +46,13 @@ angular.module('greenPiThumbApp.directives')
             .tickPadding(25);
           var yAxis = d3.axisLeft(y)
             .ticks(5);
+
+          // Define the area
+          var area = d3.area()
+              .curve(d3.curveMonotoneX)
+              .x(function(d) { return x(d.timestamp); })
+              .y0(height)
+              .y1(function(d) { return y(d.value); });   
 
           // Define the line
           var valueline = d3.line()
@@ -132,7 +139,7 @@ angular.module('greenPiThumbApp.directives')
               return "steelblue";
             else
               return "black";
-          }          
+          }                  
 
           var updateGraph = function(data, type) { 
             data.forEach(function(d) {
@@ -145,9 +152,17 @@ angular.module('greenPiThumbApp.directives')
               .append('svg')
                 .attr('width', width + margin.left + margin.right)
                 .attr('height', height + margin.top + margin.bottom)
-              .append('g')
-                .attr('transform',
-                      'translate(' + margin.left + ',' + margin.top + ')');
+              //~ .append('g')
+                //~ .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+              
+            svg.append("defs").append("clipPath")
+                .attr("id", "clip")
+              .append("rect")
+                .attr("width", width)
+                .attr("height", height);                       
+                
+            var g = svg.append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
             // Scale the range of the data            
             x.domain(d3.extent(data, function(d) { return d.timestamp; }));
@@ -157,45 +172,48 @@ angular.module('greenPiThumbApp.directives')
             ]);
 
             // Add the valueline path.
-            svg.append('path')
+            g.append('path')
               //~ .style("stroke", "red")
-              .attr('class', 'line')
-              .attr('d', valueline(data));
+              //~ .attr('class', 'line')
+              //~ .attr('d', valueline(data));
+              .datum(data)
+              .attr('class', 'area')
+              .attr('d', area(data));              
               
             // Add the scatterplot for tooltips.
-            svg.selectAll('dot')
-              .data(data)
-            .enter().append('circle')
-              .attr('r', getDotRadius() )
-              .attr('fill', getDotColor() )
-              .attr('cx', function(d) { return x(d.timestamp); })
-              .attr('cy', function(d) { return y(d.value); })
-              .on('mouseover', function(d) {
-                    div.transition()
-                      .duration(200)
-                      .style('opacity', 0.9);
-                    div.html(
-                      formatValue(d.value) + '<br />' +
-                      formatTime(d.timestamp) + '<br />' +
-                      formatDate(d.timestamp))
-                      .style('left', (d3.event.pageX + 3) + 'px')
-                      .style('top', (d3.event.pageY - 52) + 'px');
-                  })
-                .on('mouseout', function(d) {
-                  div.transition()
-                    .duration(500)
-                    .style('opacity', 0);
-                });
+            //~ g.selectAll('dot')
+              //~ .data(data)
+            //~ .enter().append('circle')
+              //~ .attr('r', getDotRadius() )
+              //~ .attr('fill', getDotColor() )
+              //~ .attr('cx', function(d) { return x(d.timestamp); })
+              //~ .attr('cy', function(d) { return y(d.value); })
+              //~ .on('mouseover', function(d) {
+                    //~ div.transition()
+                      //~ .duration(200)
+                      //~ .style('opacity', 0.9);
+                    //~ div.html(
+                      //~ formatValue(d.value) + '<br />' +
+                      //~ formatTime(d.timestamp) + '<br />' +
+                      //~ formatDate(d.timestamp))
+                      //~ .style('left', (d3.event.pageX + 3) + 'px')
+                      //~ .style('top', (d3.event.pageY - 52) + 'px');
+                  //~ })
+                //~ .on('mouseout', function(d) {
+                  //~ div.transition()
+                    //~ .duration(500)
+                    //~ .style('opacity', 0);
+                //~ });
 
             // We use major and minor ticks according to d3v4, seen here: https://stackoverflow.com/questions/21643787/d3-js-alternative-to-axis-ticksubdivide
             // Add the major x axis
-            svg.append('g')
+            g.append('g')
               .attr('class', 'x axis')
               .attr('transform', 'translate(0,' + height + ')')
               .call(xAxisMajor);
             
             // Add the minor x axis  
-            svg.append("g")
+            g.append("g")
               .attr("class", "x axis")
               .attr("transform", "translate(0," + height + ")")
               .call(xAxisMinor)              
@@ -205,7 +223,7 @@ angular.module('greenPiThumbApp.directives')
               .classed("minor", true);              
             
             // Add text label for the x axis
-            svg.append("text") 
+            g.append("text") 
               .attr("x", width / 2 )
               .attr("y", height + margin.bottom)
               .style("text-anchor", "middle")
@@ -213,19 +231,47 @@ angular.module('greenPiThumbApp.directives')
               .text("Time");              
 
             // Add the y axis
-            svg.append('g')
+            g.append('g')
               .attr('class', 'y axis')
               .call(yAxis);
               
             // Add text label for the y axis
-            svg.append("text")
+            g.append("text")
               .attr("transform", "rotate(-90)")
               .attr("y", 0 - margin.left)
               .attr("x",0 - (height / 2))
               .attr("dy", "1em")
               .style("text-anchor", "middle")
               .style("font-weight", "bold")
-              .text(getYAxisLabel());              
+              .text(getYAxisLabel());   
+              
+            var zoom = d3.zoom()
+              .scaleExtent([1, 32])
+              .translateExtent([[0, 0], [width, height]])
+              .extent([[0, 0], [width, height]])
+              .on("zoom", zoomed);                               
+              
+            // Set default time domain (x zoom range)
+            var timeDomainStart = new Date(d3.timeDay.offset(new Date(), 0).setHours(-4)); 
+            var timeDomainEnd = d3.timeDay.offset(new Date(), 0);
+            console.log("timeDomainStart:" + timeDomainStart); 
+            console.log("timeDomainEnd:" + timeDomainEnd);
+            var d0 = new Date(timeDomainStart),
+                d1 = new Date(timeDomainEnd);
+                
+            // Gratuitous intro zoom!
+            g.call(zoom).transition()
+                .duration(1500)
+                .call(zoom.transform, d3.zoomIdentity
+                    .scale(width / (x(d1) - x(d0)))
+                    .translate(-x(d0), 0));
+                    
+            function zoomed() {
+              var t = d3.event.transform, xt = t.rescaleX(x);
+              g.select(".area").attr("d", area.x(function(d) { return xt(d.timestamp); }));
+              g.select(".axis--x").call(xAxisMajor.scale(xt));
+          }                      
+              
           };
           scope.$watch('data', function(newValue) {
             if (!newValue) { return; }
