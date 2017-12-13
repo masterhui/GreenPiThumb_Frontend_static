08c15eb94617403ce538c5048c3bfb1d6f2cf478
Heiko <heikohelble@gmail.com>
2017-12-13 01:49:08 +0100

    saveIt2

--- app/directives/lineGraph.js
+++ app/directives/lineGraph.js
@@ -22,8 +22,129 @@ angular.module('greenPiThumbApp.directives')
       //template:'<div class="type"><h2>type is {{type}}</h2></div>',
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
+        
+          //~ var svg = d3.select(element[0]),
+              //~ margin = {top: 30, right: 20, bottom: 50, left: 50},
+              //~ width = 900 - margin.left - margin.right,
+              //~ height = 450 - margin.top - margin.bottom;            
+          
+          // Set the dimensions of the canvas / graph
+          var margin = {top: 30, right: 20, bottom: 50, left: 50};
+          var width = 900 - margin.left - margin.right;
+          var height = 450 - margin.top - margin.bottom;
+          
+          var parseTimestamp = d3.utcParse('%Y%m%dT%H%M%Z');   
+    
+          // Set the ranges    
+          var x = d3.scaleTime().range([0, width]);
+          var y = d3.scaleLinear().range([height, 0]);
+
+          // Define the axes
+          function xTickMajorFunc() { return d3.timeHour.every(1); }
+          var xAxisMajor = d3.axisBottom(x)
+            .ticks(xTickMajorFunc())
+            .tickFormat(d3.timeFormat('%H'));
+          var xAxisMinor = d3.axisBottom(x)
+            .ticks(d3.timeDay.every(1))
+            .tickFormat(d3.timeFormat('%a, %x'))
+            .tickSize(-height)
+            .tickPadding(25);
+          var yAxis = d3.axisLeft(y)
+            .ticks(5);
+
+          // Define the area
+          var area = d3.area()
+              .curve(d3.curveMonotoneX)
+              .x(function(d) { return x(d.timestamp); })
+              .y0(height)
+              .y1(function(d) { return y(d.value); });   
+
+          // Define the line
+          var valueline = d3.line()
+            //~ .curve(d3.curveBasis)
+            .x(function(d) { return x(d.timestamp); })
+            .y(function(d) { return y(d.value); });
+
+          // Define the div for the tooltip.
+          var div = d3.select('body').append('div')
+            .attr('class', 'tooltip')
+            .style('opacity', 0);
+
+          // Format the time and values for the tooltip.
+          var formatTime = d3.timeFormat('%I:%M %p');
+          var formatValue = d3.format('.1f');
+          var formatDate = d3.timeFormat('%Y-%m-%d');
+          
+          function getMinRange() { 
+            var retVal = 0;
             
-          var parseTimestamp = d3.utcParse('%Y%m%dT%H%M%Z');
+            if (attrs.type === "temperature")            
+              retVal = TEMPERATURE_MIN   ;
+            else if (attrs.type === "humidity")
+              retVal = HUMIDITY_MIN;
+            else if (attrs.type === "light")
+              retVal = LIGHT_MIN;
+            else if (attrs.type === "soil_moisture")
+              retVal = SOIL_MOISTURE_MIN;
+            else if (attrs.type === "water_pumped")
+              retVal = WATER_PUMPED_MIN;
+            else if (attrs.type === "water_level")
+              retVal = WATER_LEVEL_MIN;                                                
+
+            return retVal;
+          }
+      
+          function getMaxRange() {
+            var retVal = 0;
+            
+            if (attrs.type === "temperature")
+              retVal = TEMPERATURE_MAX;
+            else if (attrs.type === "humidity")
+              retVal = HUMIDITY_MAX;
+            else if (attrs.type === "light")
+              retVal = LIGHT_MAX;
+            else if (attrs.type === "soil_moisture")
+              retVal = SOIL_MOISTURE_MAX;
+            else if (attrs.type === "water_pumped")
+              retVal = WATER_PUMPED_MAX;
+            else if (attrs.type === "water_level")
+              retVal = WATER_LEVEL_MAX;                                                
+
+            return retVal;
+          }     
+          
+          function getYAxisLabel() {
+            var label = "";
+            
+            if (attrs.type === "temperature")
+              label = "Temperature [°C]";
+            else if (attrs.type === "humidity")
+              label = "Humidity [%]";
+            else if (attrs.type === "light")
+              label = "Brightness [%]";
+            else if (attrs.type === "soil_moisture")
+              label = "Soil Moisture [%]";
+            else if (attrs.type === "water_pumped")
+              label = "Pump Event [ml]";
+            else if (attrs.type === "water_level")
+              label = "Tank Level [%]";                                                
+
+            return label;
+          }
+          
+          function getDotRadius() {
+            if(attrs.type === "water_pumped") 
+              return 6.0;
+            else
+              return 1.0;
+          }
+          
+          function getDotColor() {
+            if(attrs.type === "water_pumped") 
+              return "steelblue";
+            else
+              return "black";
+          }                                       
 
           var updateGraph = function(data, type) { 
             data.forEach(function(d) {
@@ -31,138 +152,11 @@ angular.module('greenPiThumbApp.directives')
               d.value = scope.$eval(attrs.valueProperty, d);
             });
             
-          var svg = d3.select("svg"),
-              margin = {top: 30, right: 20, bottom: 50, left: 50},
-              width = +svg.attr("width") - margin.left - margin.right,
-              height = +svg.attr("height") - margin.top - margin.bottom;            
-            
-            // Set the dimensions of the canvas / graph
-            //~ var margin = {top: 30, right: 20, bottom: 50, left: 50};
-            //~ var width = 900 - margin.left - margin.right;
-            //~ var height = 450 - margin.top - margin.bottom;
-      
-            // Set the ranges    
-            var x = d3.scaleTime().range([0, width]);
-            var y = d3.scaleLinear().range([height, 0]);
-
-            // Define the axes
-            function xTickMajorFunc() { return d3.timeHour.every(1); }
-            var xAxisMajor = d3.axisBottom(x)
-              .ticks(xTickMajorFunc())
-              .tickFormat(d3.timeFormat('%H'));
-            var xAxisMinor = d3.axisBottom(x)
-              .ticks(d3.timeDay.every(1))
-              .tickFormat(d3.timeFormat('%a, %x'))
-              .tickSize(-height)
-              .tickPadding(25);
-            var yAxis = d3.axisLeft(y)
-              .ticks(5);
-              
-            var zoom = d3.zoom()
-              .scaleExtent([1, 32])
-              .translateExtent([[0, 0], [width, height]])
-              .extent([[0, 0], [width, height]])
-              .on("zoom", zoomed);                    
-
-            // Define the area
-            var area = d3.area()
-                .curve(d3.curveMonotoneX)
-                .x(function(d) { return x(d.timestamp); })
-                .y0(height)
-                .y1(function(d) { return y(d.value); });   
-
-            // Define the line
-            var valueline = d3.line()
-              //~ .curve(d3.curveBasis)
-              .x(function(d) { return x(d.timestamp); })
-              .y(function(d) { return y(d.value); });
-
-            // Define the div for the tooltip.
-            var div = d3.select('body').append('div')
-              .attr('class', 'tooltip')
-              .style('opacity', 0);
-
-            // Format the time and values for the tooltip.
-            var formatTime = d3.timeFormat('%I:%M %p');
-            var formatValue = d3.format('.1f');
-            var formatDate = d3.timeFormat('%Y-%m-%d');
-            
-            function getMinRange() { 
-              var retVal = 0;
-              
-              if (attrs.type === "temperature")            
-                retVal = TEMPERATURE_MIN   ;
-              else if (attrs.type === "humidity")
-                retVal = HUMIDITY_MIN;
-              else if (attrs.type === "light")
-                retVal = LIGHT_MIN;
-              else if (attrs.type === "soil_moisture")
-                retVal = SOIL_MOISTURE_MIN;
-              else if (attrs.type === "water_pumped")
-                retVal = WATER_PUMPED_MIN;
-              else if (attrs.type === "water_level")
-                retVal = WATER_LEVEL_MIN;                                                
-
-              return retVal;
-            }
-        
-            function getMaxRange() {
-              var retVal = 0;
-              
-              if (attrs.type === "temperature")
-                retVal = TEMPERATURE_MAX;
-              else if (attrs.type === "humidity")
-                retVal = HUMIDITY_MAX;
-              else if (attrs.type === "light")
-                retVal = LIGHT_MAX;
-              else if (attrs.type === "soil_moisture")
-                retVal = SOIL_MOISTURE_MAX;
-              else if (attrs.type === "water_pumped")
-                retVal = WATER_PUMPED_MAX;
-              else if (attrs.type === "water_level")
-                retVal = WATER_LEVEL_MAX;                                                
-
-              return retVal;
-            }     
-            
-            function getYAxisLabel() {
-              var label = "";
-              
-              if (attrs.type === "temperature")
-                label = "Temperature [°C]";
-              else if (attrs.type === "humidity")
-                label = "Humidity [%]";
-              else if (attrs.type === "light")
-                label = "Brightness [%]";
-              else if (attrs.type === "soil_moisture")
-                label = "Soil Moisture [%]";
-              else if (attrs.type === "water_pumped")
-                label = "Pump Event [ml]";
-              else if (attrs.type === "water_level")
-                label = "Tank Level [%]";                                                
-
-              return label;
-            }
-            
-            function getDotRadius() {
-              if(attrs.type === "water_pumped") 
-                return 6.0;
-              else
-                return 1.0;
-            }
-            
-            function getDotColor() {
-              if(attrs.type === "water_pumped") 
-                return "steelblue";
-              else
-                return "black";
-            }                              
-
             // Add the svg canvas
-            //~ var svg = d3.select(element[0])
-              //~ .append('svg')
-                //~ .attr('width', width + margin.left + margin.right)
-                //~ .attr('height', height + margin.top + margin.bottom)
+            var svg = d3.select(element[0])
+              .append('svg')
+                .attr('width', width + margin.left + margin.right)
+                .attr('height', height + margin.top + margin.bottom);
               //~ .append('g')
                 //~ .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
               
@@ -173,8 +167,9 @@ angular.module('greenPiThumbApp.directives')
                 .attr("height", height);                       
                 
             var g = svg.append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")"); 
+                        
+                        
             // Scale the range of the data            
             x.domain(d3.extent(data, function(d) { return d.timestamp; }));
             y.domain([
@@ -263,25 +258,35 @@ angular.module('greenPiThumbApp.directives')
             //~ console.log("timeDomainEnd:" + timeDomainEnd);
             var d0 = new Date(timeDomainStart),
                 d1 = new Date(timeDomainEnd);
-                
+            
+            console.log("updateGraph()");
+            
+            var zoom = d3.zoom()
+              .scaleExtent([1, 32])
+              .translateExtent([[0, 0], [width, height]])
+              .extent([[0, 0], [width, height]])
+              .on("zoom", zoomed);                
+              
             // Gratuitous intro zoom!
             svg.call(zoom).transition()
                 .duration(1500)
                 .call(zoom.transform, d3.zoomIdentity
                     .scale(width / (x(d1) - x(d0)))
-                    .translate(-x(d0), 0));
-                    
+                    .translate(-x(d0), 0));             
+                                  
             function zoomed() {
+              console.log("zoomed()");
               var t = d3.event.transform, xt = t.rescaleX(x);
               g.select(".area").attr("d", area.x(function(d) { return xt(d.timestamp); }));
               g.select(".axis--x").call(xAxisMajor.scale(xt));
-          }                      
-              
+            }                        
           };
+          
           scope.$watch('data', function(newValue) {
             if (!newValue) { return; }
             updateGraph(newValue);
           });
+          
         });
       }
     };
